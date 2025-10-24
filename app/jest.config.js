64b7602e0a54d580e0bfd7d3fc49bd267e94951d
Tania Roche <tania.roche@softwareone.com>
2025-10-24 08:49:00 +0100

    Adding jest packages, configuration, example function and associated tests

---
+++ app/jest.config.js
@@ -0,0 +1,6 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  testMatch: ['**/__tests__/**/*.test.(ts|tsx|js)'],
+};
