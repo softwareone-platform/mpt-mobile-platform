2abee2a9c3435657c47464e6e2e318ea5ad23d28
Balint Polgar <balint.polgar@softwareone.com>
2025-11-24 14:54:48 +0000

    wdio conf changed to use appium through hostname

--- app/wdio.conf.js
+++ app/wdio.conf.js
@@ -10,6 +10,7 @@ exports.config = {
     // ====================
     // WebdriverIO supports running e2e tests as well as unit and component tests.
     runner: 'local',
+    hostname: process.env.APPIUM_HOST || 'localhost',
     port: process.env.APPIUM_PORT || 4723,
     //
     // ==================
@@ -114,7 +115,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['appium'],
+    services: [],
 
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
