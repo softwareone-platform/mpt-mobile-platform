6a3a9426ec7029aa6b15f4380a30b25db228c5df
Tania Roche <tania.roche@softwareone.com>
2025-12-10 15:52:08 +0000

    Removing space

--- app/src/context/AccountContext.tsx
+++ app/src/context/AccountContext.tsx
@@ -79,7 +79,7 @@ export const AccountProvider = ({ children }: { children: ReactNode }) => {
     try {
       const { data } = await getSpotlightData(userId);
       const arrangedData = arrangeSpotlightData(data, SPOTLIGHT_CATEGORY);
-     
+    
       setSpotlightData(arrangedData);
       setSpotlightError(false); 
     } catch (error) {
