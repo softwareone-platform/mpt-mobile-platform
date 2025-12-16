3d77a2affbb5bb0c098ff29866a1119d9c29659a
mbeernut <maciej.biernat@softwareone.com>
2025-12-16 14:21:31 +0100

    Add Query Client

--- app/App.tsx
+++ app/App.tsx
@@ -1,17 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { AuthProvider } from '@/context/AuthContext';
 import { AccountProvider } from '@/context/AccountContext';
 import { Navigation } from '@/components/navigation';
+import { queryClient } from '@/config/queryClient';
 import './src/i18n';
 
 const App = () => {
   return (
-    <AuthProvider>
-      <AccountProvider>
-        <Navigation />
-        <StatusBar style="auto" />
-      </AccountProvider>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <AccountProvider>
+          <Navigation />
+          <StatusBar style="auto" />
+        </AccountProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   );
 };
 
