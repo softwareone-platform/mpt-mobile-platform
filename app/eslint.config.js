b27be2aa30a871917df228d52ed8cd3356fa09f3
Tania-Roche-SWO <tania.roche@softwareone.com>
2026-01-07 10:08:51 +0000

    Implementing chip component and support for five base styles (#59)
    * Implementing chip componenet and support for five base styles
    
    * Disabling lint rule for unused style for dynamic styling

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -38,7 +38,6 @@ const commonRules = {
   'react/display-name': 'off',
   'react-hooks/rules-of-hooks': 'error',
   'react-hooks/exhaustive-deps': 'warn',
-
   'react-native/no-unused-styles': 'warn',
   'react-native/no-inline-styles': 'warn',
   'react-native/no-color-literals': 'warn',
