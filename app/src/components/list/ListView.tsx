e82b210d86d81e90e51633c51b8a524ea38004da
Tania-Roche-SWO <tania.roche@softwareone.com>
2026-01-12 10:29:56 +0000

    Adding template and config for all list types (#65)
    * Adding template and config for all list types
    
    * Updating tests with real config instead of mock
    
    * Addressing PR comments

---
+++ app/src/components/list/ListView.tsx
@@ -0,0 +1,38 @@
+import { FlatList } from 'react-native';
+
+import ListItemWithStatus from '@/components/list-item/ListItemWithStatus';
+import { screenStyle } from '@/styles';
+import type { ListItemConfig } from '@/types/lists';
+import { mapToListItemProps } from '@/utils/list';
+
+type ListViewProps = {
+  data: Record<string, unknown>[];
+  config: ListItemConfig;
+  onItemPress?: (item: Record<string, unknown>) => void;
+};
+
+export function ListView({ data, config, onItemPress }: ListViewProps) {
+  if (data.length === 0) return null;
+
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={(item) => String(item[config.id])}
+      contentContainerStyle={screenStyle.containerMain}
+      renderItem={({ item, index }) => {
+        const isFirst = index === 0;
+        const isLast = index === data.length - 1;
+
+        return (
+          <ListItemWithStatus
+            {...mapToListItemProps(item, config)}
+            isFirst={isFirst}
+            isLast={isLast}
+            onPress={() => onItemPress?.(item)}
+          />
+        );
+      }}
+      showsVerticalScrollIndicator={false}
+    />
+  );
+}
