4328afde513c18935c25b39ded3c17e5bc36835d
Magdalena Komuda <magdalena.komuda@softwareone.com>
2026-01-19 17:42:36 +0100

    MPT-16069 subscriptions list view

--- app/src/screens/subscriptions/SubscriptionsScreen.tsx
+++ app/src/screens/subscriptions/SubscriptionsScreen.tsx
@@ -1,11 +1,52 @@
-import { View, Text } from 'react-native';
+import { useTranslation } from 'react-i18next';
 
-const SubscriptionsScreen = () => {
+import StatusMessage from '@/components/common/EmptyStateHelper';
+import { ListView } from '@/components/list/ListView';
+import { listItemConfigNoImage } from '@/config/list';
+import { useSubscriptions, SubscriptionsProvider } from '@/context/SubscriptionsContext';
+import { TestIDs } from '@/utils/testID';
+
+const SubscriptionsScreenContent = () => {
+  const {
+    subscriptions,
+    subscriptionsLoading,
+    subscriptionsError,
+    subscriptionsFetchingNext,
+    hasMoreSubscriptions,
+    isUnauthorised,
+    fetchSubscriptions,
+  } = useSubscriptions();
+
+  const { t } = useTranslation();
+
   return (
-    <View>
-      <Text>Subscriptions Screen</Text>
-    </View>
+    <StatusMessage
+      isLoading={subscriptionsLoading}
+      isError={!!subscriptionsError}
+      isEmpty={subscriptions.length === 0}
+      isUnauthorised={isUnauthorised}
+      loadingTestId={TestIDs.SUBSCRIPTIONS_LOADING_INDICATOR}
+      errorTestId={TestIDs.SUBSCRIPTIONS_ERROR_STATE}
+      emptyTestId={TestIDs.SUBSCRIPTIONS_EMPTY_STATE}
+      emptyTitle={t('subscriptionsScreen.emptyStateTitle')}
+      emptyDescription={t('subscriptionsScreen.emptyStateDescription')}
+    >
+      <ListView
+        data={subscriptions}
+        isFetchingNext={subscriptionsFetchingNext}
+        hasMore={hasMoreSubscriptions}
+        fetchNext={fetchSubscriptions}
+        config={listItemConfigNoImage}
+        onItemPress={(item) => console.info(item.id)}
+      />
+    </StatusMessage>
   );
 };
+
+const SubscriptionsScreen = () => (
+  <SubscriptionsProvider>
+    <SubscriptionsScreenContent />
+  </SubscriptionsProvider>
+);
 
 export default SubscriptionsScreen;
