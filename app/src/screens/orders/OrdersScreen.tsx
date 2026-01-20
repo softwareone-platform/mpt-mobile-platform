fe8261673d72edc3f244c1af1d55ddc33337a3b4
Magdalena Komuda <144448675+MagdalenaKomudaSWO@users.noreply.github.com>
2026-01-20 11:29:59 +0100

    MPT-16068 Implement Orders List View (#76)

--- app/src/screens/orders/OrdersScreen.tsx
+++ app/src/screens/orders/OrdersScreen.tsx
@@ -1,11 +1,52 @@
-import { View, Text } from 'react-native';
+import { useTranslation } from 'react-i18next';
 
-const OrdersScreen = () => {
+import StatusMessage from '@/components/common/EmptyStateHelper';
+import { ListView } from '@/components/list/ListView';
+import { listItemConfigNoImageNoSubtitle } from '@/config/list';
+import { useOrders, OrdersProvider } from '@/context/OrdersContext';
+import { TestIDs } from '@/utils/testID';
+
+const OrdersScreenContent = () => {
+  const {
+    orders,
+    ordersLoading,
+    ordersError,
+    ordersFetchingNext,
+    hasMoreOrders,
+    isUnauthorised,
+    fetchOrders,
+  } = useOrders();
+
+  const { t } = useTranslation();
+
   return (
-    <View>
-      <Text>Orders Screen</Text>
-    </View>
+    <StatusMessage
+      isLoading={ordersLoading}
+      isError={!!ordersError}
+      isEmpty={orders.length === 0}
+      isUnauthorised={isUnauthorised}
+      loadingTestId={TestIDs.ORDERS_LOADING_INDICATOR}
+      errorTestId={TestIDs.ORDERS_ERROR_STATE}
+      emptyTestId={TestIDs.ORDERS_EMPTY_STATE}
+      emptyTitle={t('ordersScreen.emptyStateTitle')}
+      emptyDescription={t('ordersScreen.emptyStateDescription')}
+    >
+      <ListView
+        data={orders}
+        isFetchingNext={ordersFetchingNext}
+        hasMore={hasMoreOrders}
+        fetchNext={fetchOrders}
+        config={listItemConfigNoImageNoSubtitle}
+        onItemPress={(item) => console.info(item.id)}
+      />
+    </StatusMessage>
   );
 };
+
+const OrdersScreen = () => (
+  <OrdersProvider>
+    <OrdersScreenContent />
+  </OrdersProvider>
+);
 
 export default OrdersScreen;
