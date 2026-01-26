7f242bb3fd68388b2b0e96d99435f3622a49dc0b
Tania Roche <tania.roche@softwareone.com>
2026-01-26 14:19:20 +0000

    removing all onPress event handlers from Details View, as there are no pages to navigate to yet

--- app/src/screens/credit-memos/CreditMemoDetailsContent.tsx
+++ app/src/screens/credit-memos/CreditMemoDetailsContent.tsx
@@ -1,69 +1,22 @@
-import { useNavigation } from '@react-navigation/native';
-import type { StackNavigationProp } from '@react-navigation/stack';
 import { useTranslation } from 'react-i18next';
 
 import CardWithHeader from '@/components/card/CardWithHeader';
 import DetailsListItem from '@/components/list-item/DetailsListItem';
 import ListItemWithLabelAndText from '@/components/list-item/ListItemWithLabelAndText';
 import type { CreditMemoDetails } from '@/types/billing';
-import type { TabParamList } from '@/types/navigation';
 
 const CreditMemoDetailsContent = ({ data }: { data: CreditMemoDetails }) => {
   const { t } = useTranslation();
-  const navigation = useNavigation<StackNavigationProp<TabParamList>>();
 
   return (
     <CardWithHeader title={t(`details.title`)}>
-      <DetailsListItem
-        label={t(`details.client`)}
-        data={data.client}
-        onPress={() => {
-          navigation.navigate('agreements');
-        }}
-      />
-      <DetailsListItem
-        label={t(`details.buyer`)}
-        data={data.buyer}
-        onPress={() => {
-          navigation.navigate('agreements');
-        }}
-      />
-      <DetailsListItem
-        label={t(`details.licencee`)}
-        data={data.licencee}
-        onPress={() => {
-          navigation.navigate('agreements');
-        }}
-      />
-      <DetailsListItem
-        label={t(`details.vendor`)}
-        data={data.vendor}
-        onPress={() => {
-          navigation.navigate('agreements');
-        }}
-      />
-      <DetailsListItem
-        label={t(`details.product`)}
-        data={data.product}
-        onPress={() => {
-          navigation.navigate('agreements');
-        }}
-      />
-      <DetailsListItem
-        label={t(`details.agreement`)}
-        data={data.agreement}
-        hideImage={true}
-        onPress={() => {
-          navigation.navigate('agreements');
-        }}
-      />
-      <DetailsListItem
-        label={t(`details.seller`)}
-        data={data.seller}
-        onPress={() => {
-          navigation.navigate('agreements');
-        }}
-      />
+      <DetailsListItem label={t(`details.client`)} data={data.client} />
+      <DetailsListItem label={t(`details.buyer`)} data={data.buyer} />
+      <DetailsListItem label={t(`details.licencee`)} data={data.licencee} />
+      <DetailsListItem label={t(`details.vendor`)} data={data.vendor} />
+      <DetailsListItem label={t(`details.product`)} data={data.product} />
+      <DetailsListItem label={t(`details.agreement`)} data={data.agreement} hideImage={true} />
+      <DetailsListItem label={t(`details.seller`)} data={data.seller} />
       <ListItemWithLabelAndText title={t(`details.currency`)} subtitle={data.price.currency} />
       <ListItemWithLabelAndText title={t(`details.documentId`)} subtitle={data.documentNo} />
       <ListItemWithLabelAndText
