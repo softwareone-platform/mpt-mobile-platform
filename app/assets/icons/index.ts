b1f75551e52a902ec9da14519264d59cb72e482f
Tania Roche <tania.roche@softwareone.com>
2026-02-05 14:51:45 +0000

    Adding remaining icons needed for navigation, removing old icons that are no longer used

--- app/assets/icons/index.ts
+++ app/assets/icons/index.ts
@@ -1,18 +1,25 @@
 import AccountBalanceWalletOutlined from './account-balance-wallet';
+import AccountTreeOutlined from './account-tree';
+import AddCommentOutlined from './add-comment';
+import AddBusinessOutlined from './add-business';
 import AlternateEmailOutlined from './alternate-email';
 import AltRouteOutlined from './alt-route';
 import ApiOutlined from './api';
-import AssignmentOutlined from './assignment';
+import AssignmentTurnedInOutlined from './assignment-turned-in';
 import BadgeOutlined from './badge';
 import BalanceOutlined from './balance';
 import CategoryOutlined from './category';
+import ChatBubbleOutlined from './chat-bubble';
+import ChecklistRtlOutlined from './checklist-rtl';
 import ContactsOutlined from './contacts';
 import CreditCardOutlined from './credit-card';
-import DescriptionOutlined from './description';
 import DocumentScannerOutlined from './document-scanner';
 import FactCheckOutlined from './fact-check';
 import FlareOutlined from './flare';
+import FormatListNumberedRtlOutlined from './format-list-numbered-rtl';
 import GroupOutlined from './group';
+import GroupsOutlined from './groups';
+import HeadsetMicOutlined from './headset-mic';
 import HowToRegOutlined from './how-to-reg';
 import InsertChartOutlined from './insert-chart';
 import InventoryTwoOutlined from './inventory-two';
@@ -20,40 +27,50 @@ import LanguageOutlined from './language';
 import ListOutlined from './list';
 import LockOutlined from './lock';
 import MailOutlined from './mail';
+import ManageHistoryOutlined from './manage-history';
 import MarkAsUnreadOutlined from './mark-as-unread';
 import MenuBookOutlined from './menu-book';
 import MoreHorizOutlined from './more-horiz';
 import NotificationsOutlined from './notifications';
+import PersonOutlined from './person';
 import PriceChangeOutlined from './price-change';
 import PriceCheckOutlined from './price-check';
 import PublicOutlined from './public';
 import ReceiptOutlined from './receipt';
 import ReceiptLongOutlined from './receipt-long';
 import RedeemOutlined from './redeem';
-import SellOutlined from './sell';
-import ShoppingBasketOutlined from './shopping-backet';
+import ShopTwoOutlined from './shop-two';
 import ShoppingCartOutlined from './shopping-cart';
 import ShoppingCartCheckoutOutlined from './shopping-cart-checkout';
 import StorefrontOutlined from './storefront';
 import SubscriptionsOutlined from './subscriptions';
+import WorkspacePremiumOutlined from './workspace-premium';
+import { group } from 'console';
 
 // Export all outlined icons with keys matching the names of filled Material Icons
 export const OutlinedIcons = {
   'account-balance-wallet': AccountBalanceWalletOutlined,
+  'account-tree': AccountTreeOutlined,
+  'add-business': AddBusinessOutlined,
+  'add-comment': AddCommentOutlined,
   'alternate-email': AlternateEmailOutlined,
   'alt-route': AltRouteOutlined,
   api: ApiOutlined,
-  assignment: AssignmentOutlined,
+  'assignment-turned-in': AssignmentTurnedInOutlined,
   badge: BadgeOutlined,
   balance: BalanceOutlined,
   category: CategoryOutlined,
+  'chat-bubble': ChatBubbleOutlined,
+  'checklist-rtl': ChecklistRtlOutlined,
   contacts: ContactsOutlined,
   'credit-card': CreditCardOutlined,
-  description: DescriptionOutlined,
   'document-scanner': DocumentScannerOutlined,
   'fact-check': FactCheckOutlined,
   flare: FlareOutlined,
+  'format-list-numbered-rtl': FormatListNumberedRtlOutlined,
   group: GroupOutlined,
+  groups: GroupsOutlined,
+  'headset-mic': HeadsetMicOutlined,
   'how-to-reg': HowToRegOutlined,
   'insert-chart': InsertChartOutlined,
   'inventory-2': InventoryTwoOutlined,
@@ -61,20 +78,22 @@ export const OutlinedIcons = {
   list: ListOutlined,
   lock: LockOutlined,
   mail: MailOutlined,
+  'manage-history': ManageHistoryOutlined,
   'mark-as-unread': MarkAsUnreadOutlined,
   'menu-book': MenuBookOutlined,
   'more-horiz': MoreHorizOutlined,
   notifications: NotificationsOutlined,
+  person: PersonOutlined,
   'price-change': PriceChangeOutlined,
   'price-check': PriceCheckOutlined,
   public: PublicOutlined,
   receipt: ReceiptOutlined,
   'receipt-long': ReceiptLongOutlined,
   redeem: RedeemOutlined,
-  sell: SellOutlined,
-  'shopping-basket': ShoppingBasketOutlined,
+  'shop-two': ShopTwoOutlined,
   'shopping-cart': ShoppingCartOutlined,
   'shopping-cart-checkout': ShoppingCartCheckoutOutlined,
   storefront: StorefrontOutlined,
   subscriptions: SubscriptionsOutlined,
+  'workspace-premium': WorkspacePremiumOutlined,
 } as const;
