50a4c84470f4ad1b8cd58f5698721ba521ebd189
Magdalena Komuda <magdalena.komuda@softwareone.com>
2026-02-16 18:02:24 +0100

    feat (agreements): adjust default filters, selected properties for list view, use common type, remove not used type #MPT-17334

--- app/src/hooks/queries/useAgreementsData.ts
+++ app/src/hooks/queries/useAgreementsData.ts
@@ -1,6 +1,6 @@
 import { usePaginatedQuery } from '@/hooks/queries/usePaginatedQuery';
 import { useAgreementApi } from '@/services/agreementService';
-import type { Agreement } from '@/types/agreement';
+import type { ListItemNoImage } from '@/types/api';
 
 export const useAgreementsData = (
   userId: string | undefined,
@@ -8,7 +8,7 @@ export const useAgreementsData = (
 ) => {
   const { getAgreements } = useAgreementApi();
 
-  return usePaginatedQuery<Agreement>({
+  return usePaginatedQuery<ListItemNoImage>({
     queryKey: ['agreements', userId, currentAccountId],
     queryFn: getAgreements,
     enabled: !!userId && !!currentAccountId,
--- app/src/types/agreement.ts
+++ app/src/types/agreement.ts
@@ -1,32 +1,6 @@
 import type { Price } from './common';
 import type { DetailsListItemValue } from './lists';
 
-export interface Agreement {
-  id: string;
-  name: string;
-  status?: string;
-
-  externalIds?: {
-    client?: string;
-  };
-  product?: Record<string, unknown>;
-  audit?: {
-    created?: {
-      at?: string;
-    };
-    updated?: {
-      at?: string;
-    };
-  };
-  seller?: {
-    address?: Record<string, unknown>;
-  };
-  listing?: Record<string, unknown>;
-  licensee?: {
-    eligibility?: Record<string, unknown>;
-  };
-}
-
 export interface AgreementData {
   id: string;
   name: string;
