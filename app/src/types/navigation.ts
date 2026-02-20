02e77f2ae9c8e462ef1a926db1ddd42088982e85
mbeernut <maciej.biernat@softwareone.com>
2026-02-20 16:31:54 +0100

    add client products

--- app/src/types/navigation.ts
+++ app/src/types/navigation.ts
@@ -29,6 +29,7 @@ export type SecondaryTabsParamList = {
   buyers: undefined;
   allBuyers: undefined;
   products: undefined;
+  clientProducts: undefined;
 };
 
 export type AppScreensParamList = {
