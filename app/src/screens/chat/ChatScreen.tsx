25edb701f0e7e134bec7711babc38a0acc0d35db
Tania Roche <tania.roche@softwareone.com>
2026-03-13 11:30:05 +0000

    Fixing merge conflicts - adding code back

--- app/src/screens/chat/ChatScreen.tsx
+++ app/src/screens/chat/ChatScreen.tsx
@@ -4,13 +4,42 @@ import { useEffect } from 'react';
 
 import { data } from './chatData';
 
-import { useSignalR } from '@/context/SignalRContext';
 import ListViewChat from '@/components/list/ListViewChat';
+import { useSignalR } from '@/context/SignalRContext';
 import { ChatItem } from '@/types/chat';
 import type { RootStackParamList } from '@/types/navigation';
 
 const ChatScreen = () => {
   const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
+
+  const { subscribe, unsubscribe, addMessageListener, isConnected } = useSignalR();
+
+  // TODO Subscribe to chat entities when connected here just for testing purposes, to be removed
+  useEffect(() => {
+    if (!isConnected) {
+      return;
+    }
+
+    const subscriptions = [
+      { moduleName: 'Helpdesk', entityName: 'Chat' },
+      { moduleName: 'Helpdesk', entityName: 'ChatMessage' },
+      { moduleName: 'Helpdesk', entityName: 'ChatParticipant' },
+    ];
+
+    void subscribe(subscriptions);
+
+    return () => {
+      void unsubscribe(subscriptions);
+    };
+  }, [isConnected, subscribe, unsubscribe]);
+
+  useEffect(() => {
+    const removeListener = addMessageListener(() => {
+      // Handle incoming messages logic here, to be implemented
+    });
+
+    return removeListener;
+  }, [addMessageListener]);
 
   // TODO: warp into loading / error handling component when API is ready
   return (
