6ed683ba7b2aa7ff2b924c056d03565e5a52661a
Tania-Roche-SWO <tania.roche@softwareone.com>
2026-04-24 09:19:40 +0100

    fix: issue with avatars showing in headers, refactoring icon avatar (#333)
    * Fixing issue with avatars showing in headers, refactoring icon avatar
    
    * Removing gray border from custom avatar
    
    * removing unused commented out import

--- app/src/constants/icons.ts
+++ app/src/constants/icons.ts
@@ -1,6 +1,6 @@
 import { MIN_TOUCH_TARGET } from './common';
 
-import { Color, Spacing } from '@/styles';
+import { Color } from '@/styles';
 
 export const DEFAULT_ICON_SIZE = 24;
 export const DEFAULT_ICON_COLOR = Color.gray.gray4;
@@ -11,8 +11,6 @@ export const DEFAULT_CHAT_AVATAR_SIZE = 48;
 export const MIN_NUMBER_OF_CHAT_AVATARS = 1;
 export const MAX_NUMBER_OF_CHAT_AVATARS = 3;
 export const DEFAULT_AVATAR_IMAGE_SHIFT = 0;
-export const HELPDESK_AVATAR_SIZE = 44;
-export const HELPDESK_AVATAR_ICON_PADDING = Spacing.spacingSmall10;
 
 const ICON_ONLY_BUTTON_PADDING = 12;
 export const ICON_ONLY_BUTTON_SIZE = DEFAULT_ICON_SIZE + ICON_ONLY_BUTTON_PADDING;
