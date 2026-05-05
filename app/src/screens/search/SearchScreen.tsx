2e0ddf62b6572b463f6a598904bb92d56ebe6e39
Tania-Roche-SWO <tania.roche@softwareone.com>
2026-05-05 09:32:34 +0100

    Search - adding filters for invoices and orders (#355)

--- app/src/screens/search/SearchScreen.tsx
+++ app/src/screens/search/SearchScreen.tsx
@@ -12,6 +12,8 @@ import type { SearchCategory } from '@/config/searchQuery';
 import { KEYBOARD_VERTICAL_OFFSET_FULL_SCREEN } from '@/constants';
 import { useDebounce } from '@/hooks/useDebounce';
 import { AgreementsList } from '@/screens/agreements/AgreementsScreen';
+import { InvoicesList } from '@/screens/invoices/InvoicesScreen';
+import { OrdersList } from '@/screens/orders/OrdersScreen';
 import { screenStyle, spacingStyle } from '@/styles';
 import { TestIDs } from '@/utils/testID';
 
@@ -34,7 +36,7 @@ const SearchScreen = () => {
           <SearchInput
             value={searchTerm}
             onChangeText={setSearchTerm}
-            placeholder={t('search.placeholder')}
+            placeholder={`${t('search.placeholder')} ${t(`searchScreen.filter.${activeCategory}`).toLocaleLowerCase()}`}
           />
         </SubHeaderContainer>
         <View>
@@ -51,6 +53,12 @@ const SearchScreen = () => {
           <View style={styles.container}>
             {activeCategory === 'agreements' && (
               <AgreementsList query={query} contentContainerStyle={styles.noPaddingTop} />
+            )}
+            {activeCategory === 'invoices' && (
+              <InvoicesList query={query} contentContainerStyle={styles.noPaddingTop} />
+            )}
+            {activeCategory === 'orders' && (
+              <OrdersList query={query} contentContainerStyle={styles.noPaddingTop} />
             )}
           </View>
         ) : (
