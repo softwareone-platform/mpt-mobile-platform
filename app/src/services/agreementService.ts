efb6715ebb34b9dc14d4e0e3b3ce281301f3c9fc
Tania Roche <tania.roche@softwareone.com>
2026-05-18 14:22:24 +0100

    Moving order by name into default query, as not all queries will be ordered, for example search is not ordered

--- app/src/services/agreementService.ts
+++ app/src/services/agreementService.ts
@@ -14,15 +14,16 @@ export function useAgreementApi() {
       limit: number = DEFAULT_PAGE_SIZE,
       query?: string,
     ): Promise<PaginatedResponse<ListItemNoImage>> => {
-      const defaultQuery = `&filter(group.buyers)&and(ne(status,"Draft"),ne(status,"Failed"))`;
+      const defaultQuery = `&filter(group.buyers)&and(ne(status,"Draft"),ne(status,"Failed"))&order=name`;
 
       const endpoint =
         `/v1/commerce/agreements` +
         `?select=-*,id,name,status` +
         `${query || defaultQuery}` +
-        `&order=name` +
         `&offset=${offset}` +
         `&limit=${limit}`;
+
+      console.info('ENDPOINT', endpoint);
 
       return api.get<PaginatedResponse<ListItemNoImage>>(endpoint);
     },
